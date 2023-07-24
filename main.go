d04f002367140384538777f5e34f2caa9396de47
Manuel Roa <manuel.roaojeda23@gmail.com>
2023-07-23 22:10:15 -0300

    s

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"api/routes"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", routes.HomeHandler)
+
+	http.ListenAndServe(":3000", r)
+}
