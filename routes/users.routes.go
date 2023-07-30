042d3994ff60308b6e6e474c38f7681803e71b2d
Manuel Roa <manuel.roaojeda23@gmail.com>
2023-07-30 09:54:12 -0300

    users

--- routes/users.routes.go
+++ routes/users.routes.go
@@ -1,23 +1,68 @@
 package routes
 
-import "net/http"
+import (
+	"api/db"
+	"api/models"
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func GetUsersHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("get users"))
+	var users []models.User
+	db.DB.Find(&users)
+	json.NewEncoder(w).Encode(&users)
 
 }
 
 func GetUserHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("get user"))
+	// w.Write([]byte("get user un user"))
+	var user models.User
+	params := mux.Vars(r)
+	db.DB.First(&user, params["id"])
+	if user.ID == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("user not found	"))
+
+	} else {
+		json.NewEncoder(w).Encode(user)
+
+	}
 
 }
 
 func PostUsersHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("post user"))
+
+	var user models.User
+	json.NewDecoder(r.Body).Decode(&user)
+	createdUser := db.DB.Create(&user)
+	err := createdUser.Error
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+	}
+
+	json.NewEncoder(w).Encode(&user)
 
 }
 
 func DeleteUsersHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("delete user"))
+
+	var user models.User
+	params := mux.Vars(r)
+	db.DB.First(&user, params["id"])
+
+	if user.ID == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("user not found	"))
+
+	} else {
+		// db.DB.Delete(&user) // marca deleted
+		db.DB.Unscoped().Delete(&user) // borra de la tabla
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("deleted	"))
+
+	}
 
 }
