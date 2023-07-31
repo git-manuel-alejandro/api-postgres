93599d134f18dea675c3d9ae0a0fb52baa08aac4
Manuel Roa <manuel.roaojeda23@gmail.com>
2023-07-31 11:06:22 -0300

    users optimized

--- routes/users.routes.go
+++ routes/users.routes.go
@@ -9,22 +9,55 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type Message struct {
+	Msg string
+}
+
+func PostUsersHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+
+	var user models.User
+	json.NewDecoder(r.Body).Decode(&user)
+	createdUser := db.DB.Create(&user)
+	err := createdUser.Error
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		m := Message{err.Error()}
+
+		json.NewEncoder(w).Encode(m)
+	} else {
+
+		json.NewEncoder(w).Encode(&user)
+	}
+
+}
+
 func GetUsersHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
 	var users []models.User
 	db.DB.Find(&users)
+	if len(users) == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		m := Message{"Not users registered"}
+		json.NewEncoder(w).Encode(&m)
+		return
+
+	}
+
 	json.NewEncoder(w).Encode(&users)
 
 }
 
 func GetUserHandler(w http.ResponseWriter, r *http.Request) {
-	// w.Write([]byte("get user un user"))
+	w.Header().Set("Content-type", "application/json")
 	var user models.User
 	params := mux.Vars(r)
 	db.DB.First(&user, params["id"])
 
 	if user.ID == 0 {
 		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte("user not found	"))
+		m := Message{"User not found"}
+		json.NewEncoder(w).Encode(&m)
 
 	} else {
 		db.DB.Model(&user).Association("Tasks").Find(&user.Tasks)
@@ -34,23 +67,8 @@ func GetUserHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func PostUsersHandler(w http.ResponseWriter, r *http.Request) {
-
-	var user models.User
-	json.NewDecoder(r.Body).Decode(&user)
-	createdUser := db.DB.Create(&user)
-	err := createdUser.Error
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(err.Error()))
-	} else {
-
-		json.NewEncoder(w).Encode(&user)
-	}
-
-}
-
 func DeleteUsersHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
 
 	var user models.User
 	params := mux.Vars(r)
@@ -58,13 +76,16 @@ func DeleteUsersHandler(w http.ResponseWriter, r *http.Request) {
 
 	if user.ID == 0 {
 		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte("user not found	"))
+
+		m := Message{"user not found"}
+		json.NewEncoder(w).Encode(m)
 
 	} else {
 		// db.DB.Delete(&user) // marca deleted
 		db.DB.Unscoped().Delete(&user) // borra de la tabla
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("deleted	"))
+		m := Message{"User id " + params["id"] + " was deleted"}
+		json.NewEncoder(w).Encode(m)
 
 	}
 
